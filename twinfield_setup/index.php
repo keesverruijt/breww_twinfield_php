69e637775a987466d199d9c39656a248221d6aa5
Kees Verruijt <kees@verruijt.net>
2023-10-25 11:40:47 +0200

    Document Twinfield API token obtain process

--- twinfield_setup/index.php
+++ twinfield_setup/index.php
@@ -4,9 +4,10 @@
   <body>
     <h1>Breww to Twinfield integration</h1>
 
-    This page asks for access to the 'Brew_Twinfield_php' application.
-
-    Click here to ask for permission:
+    <p>
+    This page asks Twinfield for access to the 'Brew_Twinfield_php' application.
+    It uses the 'indirect OAuth2' method. 
+    </p>
 
     <?php
 
@@ -29,15 +30,33 @@
         return $output;
     }
 
-    $config = \Yosymfony\Toml\Toml::ParseFile('./.breww_twinfield_php.toml');
+    $config_file = $_SERVER['DOCUMENT_ROOT'] . '/.breww_twinfield_php.toml';
+    $config = \Yosymfony\Toml\Toml::ParseFile($config_file);
     $twinfield_auth = $config['twinfield'];
-    print_r($twinfield_auth);
+    # print_r($twinfield_auth);
 
     $client_id = $twinfield_auth['clientId'];
     $redirect_uri = $twinfield_auth['redirectUri'];
     $nonce = generateSalt(16);
 
     $url = "https://login.twinfield.com/auth/authentication/connect/authorize?client_id=" . $client_id . "&redirect_uri=" . $redirect_uri . "&response_type=code&scope=openid+offline_access+twf.organisation+twf.organisationUser+twf.user&nonce=" . $nonce . "&state=tstate" . $nonce;
+  ?>
+    <p>
+    Follow the following steps to create a Twinfield Developer Client:
+    <nl>
+    <li> Go to <a href="https://developers.twinfield.com/">Twinfield Developer Portal</a> (and login using a Developer account.)</li>
+    <li> Create a new client named "<code><?php echo $twinfield_auth['clientId'] ?></code>". Choose <i>Authorization code</i> for the
+       Authorization flow and keep the Access token type set to <i>JWT</i>, and select <i>Ignore single sign on</i>.</li>
+    <li> During creation of the client, generate a new secret (press Generate new) and store this in
+       "<?php echo $config_file; ?>" in the <code>clientSecret</code> setting.</li>
+    <li> Set the Redirect URL to "<code><?php echo $redirect_uri; ?></code>".</li>
+    <li> Save the page then click the link below. This will generate a token, store this in 
+       "<?php echo $config_file; ?>" in the <code>refreshToken</code> setting.</li>
+    </nl>
+    ... and then test the synchronization.
+    </p>
+
+    Click here to ask for permission:
 
     echo "<a href='" . $url . "'>Ask permission for $client_id</a>";
   ?>
